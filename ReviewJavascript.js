4d54775ef7cc4772bb7713155fff80bfdad69f4a
AppleJuicyPie <aidenp1123@gmail.com>
2021-04-18 20:55:00 -0700

    Problem with objects

--- ReviewJavascript.js
+++ ReviewJavascript.js
@@ -1,3 +1,10 @@
 function change(){
-    document.getElementById("ToTwo").innerHTML = "This is number 2"
+    document.getElementById("ToTwo").innerHTML = "This is number 2";
+}
+function tableChange(){
+    for(i=1;i<6;i++){
+        for(j=1;j<6;j++){
+            document.getElementById("coordinate").innerHTML = i+","+j;
+        }
+    }
 }
